f53e39ac8fdd15238c69ec61d49fca7ddd9a8c23
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-20 22:18:24 -0400

    Level 1 register usernames and passwords

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -18,6 +31,34 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save((err) => {
+    err ? console.log(err) : res.render('secrets');
+  });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render('secrets');
+        }
+      }
+    }
+  });
 });
 
 const port = 3000;
