241c29be8beb41a1b188379587826016565a5e3a
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-20 21:54:07 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('Server has started successfully');
+});
