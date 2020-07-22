97ddfbdef0a7284407a2fba14e3df99e6512d814
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-22 09:58:04 -0400

    Level 3 Hashing Passwords

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -19,11 +19,6 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-});
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password'],
 });
 
 const User = new mongoose.model('User', userSchema);
@@ -43,7 +38,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save((err) => {
@@ -53,7 +48,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username }, (err, foundUser) => {
     if (err) {
